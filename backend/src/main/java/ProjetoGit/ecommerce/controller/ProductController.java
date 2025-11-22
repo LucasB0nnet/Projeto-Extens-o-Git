9515e1d2d29b4d32aa92f62bb25f28905c79c343
LucasB0nnet <lucasbonnet688@gmail.com>
2025-11-22 00:04:46 -0300

    feat:Implementação controller delete e update

--- backend/src/main/java/ProjetoGit/ecommerce/controller/ProductController.java
+++ backend/src/main/java/ProjetoGit/ecommerce/controller/ProductController.java
@@ -5,9 +5,11 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -46,5 +48,24 @@ public class ProductController {
     public ResponseEntity<Product> createProduct(@RequestBody Product product) {
         productService.create(product);
         return ResponseEntity.status(HttpStatus.CREATED).body(product);
+    }
+    
+    @PutMapping("/{id}")
+    public ResponseEntity<Product> updateProduct(@PathVariable long id, @RequestBody Product product) {
+        if (productService.findById(id) == null) {
+            return ResponseEntity.notFound().build();
+        }
+        productService.update(product);
+        return ResponseEntity.ok(product);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteProduct(@PathVariable long id) {
+        Product product = productService.findById(id);
+        if (product == null) {
+            return ResponseEntity.notFound().build();
+        }
+        productService.delete(product);
+        return ResponseEntity.noContent().build();
     }
 }
