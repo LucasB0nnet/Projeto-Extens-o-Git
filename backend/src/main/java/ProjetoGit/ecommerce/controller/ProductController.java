a8b48513a8a13edd6866463ed735a4834c52df1e
LucasB0nnet <lucasbonnet688@gmail.com>
2025-11-21 23:56:06 -0300

    feat:Implementação controller getAllProduts

--- backend/src/main/java/ProjetoGit/ecommerce/controller/ProductController.java
+++ backend/src/main/java/ProjetoGit/ecommerce/controller/ProductController.java
@@ -1,5 +1,30 @@
 package ProjetoGit.ecommerce.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import ProjetoGit.ecommerce.entity.Product;
+import ProjetoGit.ecommerce.service.ProductService;
+
+@RestController
+@RequestMapping(value = "api/products/")
 public class ProductController {
 
+	@Autowired
+	private ProductService productService;
+
+    public ProductController(ProductService productService) {
+        this.productService = productService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Product>> getAllProducts() {
+        List<Product> products = productService.findAll();
+        return ResponseEntity.ok(products);
+    }
 }
