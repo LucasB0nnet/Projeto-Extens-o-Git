daae70ea3962b37294e58ce8db5f46075ae1a0e9
Alef Luciano <alef.luciano@gmail.com>
2025-11-21 23:18:28 -0300

    feat:Implementação da entidade do product

--- backend/src/main/java/ProjetoGit/ecommerce/entity/Product.java
+++ backend/src/main/java/ProjetoGit/ecommerce/entity/Product.java
@@ -1,5 +1,57 @@
 package ProjetoGit.ecommerce.entity;
 
-public class Product {
+import jakarta.persistence.*;
 
+@Entity
+@Table(name = "product")
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String name;
+    private String description;
+    private String category;
+    private double price;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
 }
+
+
