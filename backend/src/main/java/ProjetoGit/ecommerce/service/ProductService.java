cba176de488ba74a22caf1ef87fe230ea25a3a76
Alef Luciano <alef.luciano@gmail.com>
2025-11-21 23:31:49 -0300

    feat:Implementação do service find all e find by Id

--- backend/src/main/java/ProjetoGit/ecommerce/service/ProductService.java
+++ backend/src/main/java/ProjetoGit/ecommerce/service/ProductService.java
@@ -1,5 +1,27 @@
 package ProjetoGit.ecommerce.service;
 
+import ProjetoGit.ecommerce.entity.Product;
+import ProjetoGit.ecommerce.repository.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class ProductService {
+
+    @Autowired
+    private ProductRepository productRepository;
+    public ProductService(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+    public List<Product> findAll() {
+        return productRepository.findAll();
+    }
+
+    public Product findById(long id) {
+        Product product = productRepository.findById(id).orElse(null);
+        return product;
+    }
 
 }
